b846370b31b5ca621e93ea6f571f31032a7c2e45
vlkan <thkvolkanonder@gmail.com>
2022-08-23 22:51:58 +0300

    add new business rules

--- src/demoProjects/rentACar/Application/Features/Brands/Commands/CreateBrand/CreateBrandCommand.cs
+++ src/demoProjects/rentACar/Application/Features/Brands/Commands/CreateBrand/CreateBrandCommand.cs
@@ -5,6 +5,7 @@ using System.Security.AccessControl;
 using System.Text;
 using System.Threading.Tasks;
 using Application.Features.Brands.DTOs;
+using Application.Features.Brands.Rules;
 using Application.Services.Repositories;
 using AutoMapper;
 using Domain.Entities;
@@ -20,16 +21,19 @@ public class CreateBrandCommand : IRequest<CreatedBrandDto>
     {
         private readonly IBrandRepository _brandRepository;
         private readonly IMapper _mapper;
+        private readonly BrandBusinessRules _brandBusinessRules;
 
-
-        public CreateBrandCommandHandler(IBrandRepository brandRepository, IMapper mapper)
+        public CreateBrandCommandHandler(IBrandRepository brandRepository, IMapper mapper, BrandBusinessRules brandBusinessRules)
         {
             _brandRepository = brandRepository;
             _mapper = mapper;
+            _brandBusinessRules = brandBusinessRules;
         }
 
         public async Task<CreatedBrandDto> Handle(CreateBrandCommand request, CancellationToken cancellationToken)
         {
+            await _brandBusinessRules.BrandNameCanNotBeDuplicatedWhenInserted(request.Name);
+
             Brand mappedBrand = _mapper.Map<Brand>(request);
             Brand createdBrand = await _brandRepository.AddAsync(mappedBrand);
             CreatedBrandDto createdBrandDto = _mapper.Map<CreatedBrandDto>(createdBrand);
