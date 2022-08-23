6c0a4611d99f433c0fff04167bab3f3a7f10e204
vlkan <thkvolkanonder@gmail.com>
2022-08-23 21:53:46 +0300

    CQRS init

--- src/demoProjects/rentACar/Application/Features/Brands/Commands/CreateBrand/CreateBrandCommand.cs
+++ src/demoProjects/rentACar/Application/Features/Brands/Commands/CreateBrand/CreateBrandCommand.cs
@@ -1,13 +1,41 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.AccessControl;
 using System.Text;
 using System.Threading.Tasks;
+using Application.Features.Brands.DTOs;
+using Application.Services.Repositories;
+using AutoMapper;
+using Domain.Entities;
+using MediatR;
 
 namespace Application.Features.Brands.Commands.CreateBrand;
 
-public class CreateBrandCommand
+public class CreateBrandCommand : IRequest<CreatedBrandDto>
 {
+    public string Name { get; set; }
 
+    public class CreateBrandCommandHandler : IRequestHandler<CreateBrandCommand, CreatedBrandDto>
+    {
+        private readonly IBrandRepository _brandRepository;
+        private readonly IMapper _mapper;
+
+
+        public CreateBrandCommandHandler(IBrandRepository brandRepository, IMapper mapper)
+        {
+            _brandRepository = brandRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<CreatedBrandDto> Handle(CreateBrandCommand request, CancellationToken cancellationToken)
+        {
+            Brand mappedBrand = _mapper.Map<Brand>(request);
+            Brand createdBrand = await _brandRepository.AddAsync(mappedBrand);
+            CreatedBrandDto createdBrandDto = _mapper.Map<CreatedBrandDto>(createdBrand);
+
+            return createdBrandDto;
+        }
+    }
 }
 
