183c1224b86ab67a61519861be10af9fa381195a
vlkan <thkvolkanonder@gmail.com>
2022-08-23 20:41:12 +0300

    refactoring

--- src/demoProjects/templateProject/WebAPI/Controllers/BaseController.cs
+++ src/demoProjects/templateProject/WebAPI/Controllers/BaseController.cs
@@ -1,13 +1,12 @@
 ﻿using MediatR;
 using Microsoft.AspNetCore.Mvc;
 
-namespace WebAPI.Controllers
+namespace WebAPI.Controllers;
+
+public class BaseController : ControllerBase
 {
-    public class BaseController : ControllerBase
-    {
-        protected IMediator? Mediator => _mediator ??= HttpContext.RequestServices.GetService<IMediator>();
-        private IMediator? _mediator;
+    protected IMediator? Mediator => _mediator ??= HttpContext.RequestServices.GetService<IMediator>();
+    private IMediator? _mediator;
 
         
-    }
 }
