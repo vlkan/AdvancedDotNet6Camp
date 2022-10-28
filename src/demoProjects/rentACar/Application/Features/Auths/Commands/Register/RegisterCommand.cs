7363325c3f23f7b3850912e335ebe385a7c48d8b
vlkan <thkvolkanonder@gmail.com>
2022-10-28 11:57:26 +0300

    jwt init

---
+++ src/demoProjects/rentACar/Application/Features/Auths/Commands/Register/RegisterCommand.cs
@@ -0,0 +1,25 @@
+﻿using Application.Features.Auths.DTOs;
+using Application.Features.Auths.Rules;
+using Application.Services.Repositories;
+using Core.Security.Dtos;
+using MediatR;
+
+namespace Application.Features.Auths.Commands.Register
+{
+    public class RegisterCommand : IRequest<RegisteredDto>
+    {
+        public UserForRegisterDto  UserForRegisterDto { get; set; }
+        public string IPAddress { get; set; }
+
+        public class UserForRegisterDtoHandler : IRequestHandler<RegisterCommand, RegisteredDto>
+        {
+            private readonly AuthBusinessRules _authBusinessRules;
+            private readonly IUserRepository _userRepository;
+
+            public Task<RegisteredDto> Handle(RegisterCommand request, CancellationToken cancellationToken)
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+}
